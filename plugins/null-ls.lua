ce3a82ab65025ed8c7aefbbd188490af84c469ce
Antonin Januska <antonin@antjanus.com>
2023-06-21 10:32:54 -0600

    scala setup

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,24 +1,27 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	opts = function(_, config)
-		-- config variable is the default configuration table for the setup function call
-		local null_ls = require "null-ls"
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    local null_ls = require "null-ls"
 
-		-- Check supported formatters and linters
-		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    -- Check supported formatters and linters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 
-		config.sources = {
-			null_ls.builtins.formatting.prettierd.with {
-				extra_args = { "--prose-wrap", "always" },
-			},
-			null_ls.builtins.diagnostics.eslint,
-			null_ls.builtins.formatting.stylua.with {
-				extra_args = { "--column-width", "80" },
-			},
-			null_ls.builtins.formatting.black,
-		}
+    config.sources = {
+      null_ls.builtins.formatting.prettierd.with {
+        extra_args = { "--prose-wrap", "always" },
+      },
+      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.formatting.stylua.with {
+        extra_args = { "--column-width", "80" },
+      },
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.scalafmt,
+    }
 
-		return config -- return final config table
-	end,
+    config.default_timeout = 5000
+
+    return config -- return final config table
+  end,
 }
